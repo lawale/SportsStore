0eefbaf66f76f741ed5d01c822acc1d49f2d46e6
Olawale Lawal <lawale99@gmail.com>
2019-05-02 14:28:23 +0100

    Implemented Checkout feature

---
+++ SportsStore/Controllers/OrderController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Controllers
+{
+    public class OrderController : Controller
+    {
+        [HttpGet]
+        public ViewResult Checkout() => View();
+
+        [HttpPost]
+        public ViewResult Checkout(Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                return View();
+            }
+            else
+                return View();
+        }
+    }
+}
